976d4d12f8ac57c058914c53b6eafac3eed84414
john <john@john-System-Product-Name.(none)>
2012-09-19 15:46:38 +0200

    commit

--- convertPlancheToPdf.php
+++ convertPlancheToPdf.php
@@ -6,6 +6,9 @@ include("fpdf17/fpdf.php");
 
 $pdf = new FPDF();
 
+define("LARGEUR_PAGE", 190);
+define("HAUTEUR_PAGE", 250);
+
 $iterator = new DirectoryIterator("planche/");
 
 
@@ -16,7 +19,7 @@ $iterator = new DirectoryIterator("planche/");
             list($w, $h) = getimagesize($fileinfo->getPathname());
             
             // Étape 1 :
-            $NouvelleLargeur = 190;
+            $NouvelleLargeur = LARGEUR_PAGE;
              
             // Étape 2 :
             $Reduction = ( ($NouvelleLargeur * 100)/$w );
@@ -24,8 +27,33 @@ $iterator = new DirectoryIterator("planche/");
             // Étape 3 :
             $NouvelleHauteur = ( ($h * $Reduction)/100 );
             
-            $pdf->Image($fileinfo->getPathname(), null, null, $NouvelleLargeur, $NouvelleHauteur);
-	    $pdf->AddPage();
+            $nombrePage = ceil($nouvelleHauteur/HAUTEUR_PAGE);
+            
+            for($p = 1; $p<= $nombrePage; $p++)
+            {
+                if($p == $nombrePage)
+                {
+                    $ht = $NouvelleHauteur - (HAUTEUR_PAGE * ($nombrePage -1) );
+                }
+                else
+                {
+                    $ht = HAUTEUR_PAGE;
+                }
+                
+                $position_y = HAUTEUR_PAGE * ( $p - 1);
+                
+                $file = "tmp/".uniqid().".jpg";
+                
+                $img_source = imagecreatefromjpeg($fileinfo->getPathname());
+                $img_destination = imagecreatetruecolor();
+                
+                imagecopyresampled($img_destination, $img_source, 0, 0, 0, $position_y, LARGEUR_PAGE, $ht, LARGEUR_PAGE, $ht);
+                
+                imagejpeg($img_destination, $file, 100);
+                
+                $pdf->Image($file, null, null, $NouvelleLargeur, $NouvelleHauteur);
+                $pdf->AddPage();
+            }
         }
   }
   
